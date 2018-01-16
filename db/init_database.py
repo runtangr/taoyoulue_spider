e0fc6a9901e85e54ca8bc1a46b147e03385e42ab
tangrui <1048393815@qq.com>
2018-01-16 11:45:17 +0800

    add docker --link

--- db/init_database.py
+++ db/init_database.py
@@ -5,8 +5,8 @@ from mongoengine import connect
 # init mongodb, redis
 connect(host='10.3.131.137', port=27037, db='mc', connect=False)
 
-pool_auMessage724 = redis.ConnectionPool(host='127.0.0.1', port=6379, db=3)
+pool_auMessage724 = redis.ConnectionPool(host='redis', port=6379, db=3)
 r_auMessage724 = redis.StrictRedis(connection_pool=pool_auMessage724)
 
-pool_A_DxtInformation = redis.ConnectionPool(host='127.0.0.1', port=6379, db=4)
+pool_A_DxtInformation = redis.ConnectionPool(host='redis', port=6379, db=4)
 r_A_DxtInformation = redis.StrictRedis(connection_pool=pool_A_DxtInformation)
