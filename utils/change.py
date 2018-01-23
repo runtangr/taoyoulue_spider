36f3d9938e8b984432361cc50b1092e76b5f5a84
tangrui <1048393815@qq.com>
2018-01-23 11:23:07 +0800

    modify time to utctime

--- utils/change.py
+++ utils/change.py
@@ -3,11 +3,12 @@ import datetime
 
 
 def datetime2lc(time):
+    utc_time = time - datetime.timedelta(hours=8)
     lc_date = {}
-    time_str = datetime.datetime.strftime(time, "%Y-%m-%dT%H:%M:%S.%f")[:-3] + 'Z'
-
-    lc_date["__type"] = "Date"
+    time_str = datetime.datetime.strftime(utc_time,
+                                          "%Y-%m-%dT%H:%M:%S.%f")[:-3] + 'Z'
     lc_date["iso"] = time_str
+    lc_date["__type"] = "Date"
 
     return lc_date
 
